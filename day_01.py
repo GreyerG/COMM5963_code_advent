1a9ac7410be678fbe8968495ef8114d8669347e2
Grey <413255982@qq.com>
2026-02-08 02:36:43 +0800

    day_01

--- day_01.py
+++ day_01.py
@@ -1,16 +1,31 @@
-# 这是一个示例 Python 脚本。
 
-# 按 ⌃R 执行或将其替换为您的代码。
-# 按 双击 ⇧ 在所有地方搜索类、文件、工具窗口、操作和设置。
+def count_floor(input_text):
+    count = 0
+    for char in input_text:
+        if char == '(':
+            count += 1
+        if char == ')':
+            count -= 1
+    print(count)
 
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 ⌘F8 切换断点。
+def count_basement(input_text):
+    count = 0
+    count_char = 0
+    for char in input_text:
+        if char == '(':
+            count += 1
+            count_char += 1
+        if char == ')':
+            count -= 1
+            count_char += 1
+        if count == -1:
+            break
+    print(count_char)
 
-
-# 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    with open('input/day_01.txt','r') as file_object:
+        input_text = file_object.read()
+    count_floor(input_text)
+    count_basement(input_text)
 
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
